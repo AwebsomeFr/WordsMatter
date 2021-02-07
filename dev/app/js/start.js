c6343afa9a96b8ab1d03642ee45a5ae98e838e3f
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-02-07 09:18:22 +0100

    Add text on the connexion indicator (more accessible).

--- dev/app/js/start.js
+++ dev/app/js/start.js
@@ -38,6 +38,32 @@
 		]
 	});
 
+	// Status.
+	UI.status = chess({
+		type: 'div',
+		attributes: {
+			id: 'status'
+		},
+		html: '<svg viewBox="0 0 24 24"><path d="M14 12h-4v-12h4v12zm4.213-10.246l-1.213 1.599c2.984 1.732 5 4.955 5 8.647 0 5.514-4.486 10-10 10s-10-4.486-10-10c0-3.692 2.016-6.915 5-8.647l-1.213-1.599c-3.465 2.103-5.787 5.897-5.787 10.246 0 6.627 5.373 12 12 12s12-5.373 12-12c0-4.349-2.322-8.143-5.787-10.246z"/></svg>',
+		children: [
+			{
+				type: 'p',
+				attributes: {
+					class: "online"
+				},
+				text: LAB.bt.online
+
+			},
+			{
+				type: 'p',
+				attributes: {
+					class: "offline"
+				},
+				text: LAB.bt.offline
+			}
+		]
+	});
+
 	// Main nav.
 	UI.nav = chess({
 		type: 'nav',
@@ -51,7 +77,7 @@
 					title: LAB.bt.menu,
 					onclick: "toggleMenu(true)" 
 				},
-				html: '<svg viewBox="0 0 24 24"><path d="M14 12h-4v-12h4v12zm4.213-10.246l-1.213 1.599c2.984 1.732 5 4.955 5 8.647 0 5.514-4.486 10-10 10s-10-4.486-10-10c0-3.692 2.016-6.915 5-8.647l-1.213-1.599c-3.465 2.103-5.787 5.897-5.787 10.246 0 6.627 5.373 12 12 12s12-5.373 12-12c0-4.349-2.322-8.143-5.787-10.246z"/></svg>' + LAB.bt.menu,
+				text: LAB.bt.menu
 			},
 			{
 				type: 'div',
@@ -278,6 +304,7 @@
 
 	document.body.appendChild(UI.notice);
 	document.body.appendChild(UI.dial);
+	document.body.appendChild(UI.status);
 	UI.main.appendChild(UI.nav);
 	UI.main.appendChild(UI.output);
 	UI.main.appendChild(UI.input);
