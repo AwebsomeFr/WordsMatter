61e3b63db33e8e2a16b7963f636d243f25e22cd5
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-02-13 14:44:22 +0100

    Add a gallery for handling pictures.

---
+++ prod/api/deleteG.php
@@ -0,0 +1,2 @@
+<?php
+ ini_set('display_errors', true); ini_set('html_errors', false); error_reporting(E_ALL); if(isset($_POST['picture']) && isset($_POST['editorId'])) { require './config.php'; if($_POST['editorId'] === EDITOR_ID) { if( unlink(GALLERY_DIR . '/thumbs/' . $_POST['picture']) && unlink(GALLERY_DIR . '/normals/' . $_POST['picture']) ) { echo 'success'; } } }
