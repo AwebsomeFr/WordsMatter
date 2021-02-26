999aaf7f938c84afdf3845ae79ce7a4f62c6948d
Julien Wilhelm | Awebsome <contact@awebsome.fr>
2021-02-26 10:08:52 +0100

    Add redirection to the corresponding documentation between fr/en (not written yet).

--- dev/app/js/start.js
+++ dev/app/js/start.js
@@ -174,7 +174,7 @@
 					{
 						type: 'button',
 						attributes: {
-							onclick: "window.open('./help.html')"
+							onclick: "window.open('./help." + LANG + ".html')"
 						},
 						html: '<span>?</span>' + LAB.bt.doc,
 					},
